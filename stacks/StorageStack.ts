19c63b2d28d4c77bbf855210d2f591dcf529ec14
zhiyiyi <chenzhiyi401@gmail.com>
2023-10-07 16:33:10 -0400

    Create DynamoDB table

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,16 @@
+import { StackContext, Table } from "sst/constructs";
+
+export function StorageStack({ stack, app }: StackContext) {
+    const table = new Table(stack, "Users", {
+        fields: {
+            id: "string",
+            name: "string",
+            email: "string"
+        },
+        primaryIndex: { partitionKey: "id", sortKey: "email" }
+    });
+
+    return {
+        table
+    }
+}
